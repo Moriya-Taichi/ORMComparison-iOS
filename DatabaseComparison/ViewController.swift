47f5af2e67971d6e918908d6f8f40aee09c0c90c
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-06 05:14:37 +0900

    Initial Commit

---
+++ DatabaseComparison/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatabaseComparison
+//
+//  Created by Mori on 2020/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
