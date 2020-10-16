bcd303ab76d402bc6944d8bd21fe5a46d6ec5830
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-16 19:22:50 +0900

    implement: some store type

--- DatabaseComparison/Stores/FMDB/FMDBStoreType.swift
+++ DatabaseComparison/Stores/FMDB/FMDBStoreType.swift
@@ -1,8 +1,12 @@
 
-import Foundation
-
-protocol FMDBStore: Store {
+protocol FMDBStoreType: StoreType {
     var databaseWrapper: FMDBDatabaseWrapeer { get }
 
     init (databaseWrapper: FMDBDatabaseWrapeer)
 }
+
+protocol FMDBShopStoreType: FMDBStoreType where StoredObjectType == Shop {}
+
+protocol FMDBOwnerStoreType: FMDBStoreType where StoredObjectType == Owner {}
+
+protocol FMDBProductStoreType: FMDBStoreType where StoredObjectType == Product {}
