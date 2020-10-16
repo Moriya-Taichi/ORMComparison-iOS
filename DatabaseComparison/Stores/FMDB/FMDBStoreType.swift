523b96af1fef877a4e538e14749b91d8e8bcbd11
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-16 19:11:01 +0900

    Delete and add some filew

---
+++ DatabaseComparison/Stores/FMDB/FMDBStoreType.swift
@@ -0,0 +1,8 @@
+//
+//  FMDBStoreType.swift
+//  DatabaseComparison
+//
+//  Created by Mori on 2020/10/16.
+//
+
+import Foundation
---
+++ DatabaseComparison/Stores/StoreType.swift
@@ -0,0 +1,8 @@
+//
+//  StoreType.swift
+//  DatabaseComparison
+//
+//  Created by Mori on 2020/10/16.
+//
+
+import Foundation
