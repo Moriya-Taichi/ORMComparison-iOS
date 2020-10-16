50ffc3d41f6ba9b34a8372b7264a002b291f20de
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-16 16:17:26 +0900

    implement: move to derectory

--- DatabaseComparison/Stores/FMDB/FMDBDatabaseWrapeer.swift
+++ DatabaseComparison/Stores/FMDB/FMDBDatabaseWrapeer.swift
@@ -5,4 +5,26 @@
 //  Created by Mori on 2020/10/16.
 //
 
-import Foundation
+import FMDB
+
+final class FMDBDatabaseWrapeer {
+
+    let database: FMDatabase
+
+    init () {
+        let databasePath = try! FileManager.default
+                .url(
+                    for: .applicationSupportDirectory,
+                    in: .userDomainMask,
+                    appropriateFor: nil,
+                    create: true
+                )
+                .appendingPathComponent("test.sqlite")
+        database = FMDatabase(url: databasePath)
+        database.open()
+    }
+
+    deinit {
+        database.close()
+    }
+}
