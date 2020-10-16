3343927d1e47e191c648cebeefa8b1930ecf45fb
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-16 12:11:56 +0900

    implement: Base CoreData Store

--- DatabaseComparison/Stores/CoreDataStore.swift
+++ DatabaseComparison/Stores/CoreDataStore.swift
@@ -1,8 +1,71 @@
-//
-//  CoreDataStore.swift
-//  DatabaseComparison
-//
-//  Created by Mori on 2020/10/07.
-//
-
 import Foundation
+import CoreData
+
+final class CoreDataStore {
+
+    private let container: NSPersistentContainer
+
+    init (container: NSPersistentContainer) {
+        self.container = container
+    }
+
+    func create<T: NSManagedObject>() -> T {
+        let context = container.viewContext
+        return T(context: context)
+    }
+
+    func insert<T: NSManagedObject>(object: T) {
+        let context = container.viewContext
+        context.insert(object)
+        saveContext()
+    }
+
+    func delete<T: NSManagedObject>(object: T) {
+        let context = container.viewContext
+        context.delete(object)
+        saveContext()
+    }
+
+    func deleteAll<T: NSManagedObject>(objectType: T.Type) {
+        let context = container.viewContext
+        let fetchRequest = objectType.fetchRequest()
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        }
+        catch {
+            print(error)
+        }
+    }
+
+    func read<T: NSManagedObject>() -> NSFetchedResultsController<T> {
+        let context = container.viewContext
+        let request: NSFetchRequest<T> = .init(entityName: String(describing: T.self))
+        let controller = NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        do {
+            try controller.performFetch()
+        }
+        catch {
+            print(error)
+        }
+        return controller
+    }
+
+    func saveContext() {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+}
