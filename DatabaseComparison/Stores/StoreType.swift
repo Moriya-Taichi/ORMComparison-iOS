c4af9d329bb8be3a2e6f273e65b0650b0732fe82
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-16 19:11:25 +0900

    implement: Store Type

--- DatabaseComparison/Stores/StoreType.swift
+++ DatabaseComparison/Stores/StoreType.swift
@@ -1,8 +1,9 @@
-//
-//  StoreType.swift
-//  DatabaseComparison
-//
-//  Created by Mori on 2020/10/16.
-//
 
-import Foundation
+protocol Store {
+    associatedtype StoredObjectType
+
+    func create(object: StoredObjectType)
+    func read() -> [StoredObjectType]?
+    func update(object: StoredObjectType)
+    func delete(object: StoredObjectType)
+}
