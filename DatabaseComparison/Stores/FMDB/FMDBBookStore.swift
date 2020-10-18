1ac6728153474dc98b24fbcdbae2cd55a1494855
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-18 12:39:22 +0900

    fix: sql and function

--- DatabaseComparison/Stores/FMDB/FMDBBookStore.swift
+++ DatabaseComparison/Stores/FMDB/FMDBBookStore.swift
@@ -1,20 +1,58 @@
 
 import FMDB
 
-final class FMDBBookStore: FMDBBookStoreType {
+final class FMDBBookStore {
 
-    let databaseWrapper: FMDBDatabaseWrapeer
+    private let databaseWrapper: FMDBDatabaseWrapeer
+    private let bookTagStore: FMDBBookTagStore
 
-    init(databaseWrapper: FMDBDatabaseWrapeer) {
+    private let createTableSQL =
+        "CREATE TABLE IF NOT EXISTS books (" +
+        "id INTEGER PRIMARY KEY, " +
+        "name TEXT, " +
+        "price INTEGER, " +
+        "publisher_id TEXT" +
+      ");"
+
+    private let insertSQL =
+        "INSERT INTO " +
+        "owner (name, price, publisher_id) " +
+        "VALUES " +
+        "(?, ?, ?);"
+
+    private let selectSQL =
+        "SELECT " +
+        "id, name, age, profile " +
+        "FROM " +
+        "owner;" +
+        "ORDER BY name;"
+
+    private let updateSQL =
+        "UPDATE " +
+        "owner " +
+        "SET " +
+        "name = ?, age = ?, profile = ? " +
+        "WHERE " +
+        "id = ?;"
+
+    private let deleteSQL =
+        "DELETE FROM owner WHERE id = ?;"
+
+    init(
+        databaseWrapper: FMDBDatabaseWrapeer,
+        bookTagStore: FMDBBookTagStore
+    ) {
         self.databaseWrapper = databaseWrapper
+        self.bookTagStore = bookTagStore
     }
 
     func create(object: Book) {
 
     }
 
-    func read() -> [Book]? {
-
+    func read() -> [Book] {
+        var result: [Book] = []
+        return result
     }
 
     func update(object: Book) {
