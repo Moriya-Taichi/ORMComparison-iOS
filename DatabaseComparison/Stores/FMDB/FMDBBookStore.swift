4f857682a92b9fcd9b9fa15c79ea982b69539523
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-18 21:14:38 +0900

    implement: load by publisher ids

--- DatabaseComparison/Stores/FMDB/FMDBBookStore.swift
+++ DatabaseComparison/Stores/FMDB/FMDBBookStore.swift
@@ -4,46 +4,43 @@ import FMDB
 final class FMDBBookStore {
 
     private let databaseWrapper: FMDBDatabaseWrapeer
-    private let bookTagStore: FMDBBookTagStore
 
     private let createTableSQL =
         "CREATE TABLE IF NOT EXISTS books (" +
         "id INTEGER PRIMARY KEY, " +
         "name TEXT, " +
         "price INTEGER, " +
-        "publisher_id TEXT" +
+        "publisher_id INTEGER" +
       ");"
 
     private let insertSQL =
         "INSERT INTO " +
-        "owner (name, price, publisher_id) " +
+        "books (id, name, price, publisher_id) " +
         "VALUES " +
-        "(?, ?, ?);"
+        "(?, ?, ?, ?);"
 
     private let selectSQL =
         "SELECT " +
-        "id, name, age, profile " +
+        "id, name, price, publisher_id " +
         "FROM " +
-        "owner;" +
-        "ORDER BY name;"
+        "books;" +
+        "ORDER BY id;"
 
     private let updateSQL =
         "UPDATE " +
-        "owner " +
+        "books " +
         "SET " +
-        "name = ?, age = ?, profile = ? " +
+        "name = ?, price = ?, publisher_id = ? " +
         "WHERE " +
         "id = ?;"
 
     private let deleteSQL =
-        "DELETE FROM owner WHERE id = ?;"
+        "DELETE FROM books WHERE id = ?;"
 
     init(
-        databaseWrapper: FMDBDatabaseWrapeer,
-        bookTagStore: FMDBBookTagStore
+        databaseWrapper: FMDBDatabaseWrapeer
     ) {
         self.databaseWrapper = databaseWrapper
-        self.bookTagStore = bookTagStore
     }
 
     func create(object: Book) {
@@ -53,6 +50,27 @@ final class FMDBBookStore {
     func read() -> [Book] {
         var result: [Book] = []
         return result
+    }
+
+    func readBooksWithIDByPublisherIDs(_ ids:[Int]) -> [(book: Book, id: Int)] {
+        var books: [(Book, Int)] = []
+        let params = Array(repeating: "?", count: ids.count).joined(separator: ",")
+        let query = "SELECT * FROM books WHERE publisher_id IN (" + params + ");"
+        if
+            let result = try? databaseWrapper.executeQuery(
+                query,
+                values: ids
+            ) {
+            while result.next() {
+                let book = Book(
+                    id: result.long(forColumnIndex: 0),
+                    name: result.string(forColumnIndex: 1) ?? "",
+                    price: result.long(forColumnIndex: 2)
+                )
+                books.append((book, result.long(forColumnIndex: 3)))
+            }
+        }
+        return books
     }
 
     func update(object: Book) {
