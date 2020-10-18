d9c4ad618d6359bc772f979723525daab287e57a
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-18 12:19:32 +0900

    implement: execute wrapper

--- DatabaseComparison/Stores/FMDB/FMDBDatabaseWrapeer.swift
+++ DatabaseComparison/Stores/FMDB/FMDBDatabaseWrapeer.swift
@@ -2,7 +2,7 @@ import FMDB
 
 final class FMDBDatabaseWrapeer {
 
-    let database: FMDatabase
+    private let database: FMDatabase
 
     init () {
         let databasePath = try! FileManager.default
@@ -18,6 +18,19 @@ final class FMDBDatabaseWrapeer {
     }
 
     deinit {
+        database.close()
+    }
+
+    func executeQuery(_ sql: String, values: [Any]?) throws -> FMResultSet {
+        database.open()
+        let result = try database.executeQuery(sql, values: values)
+        database.close()
+        return result
+    }
+
+    func executeUpdate(_ sql: String, values: [Any]?) throws {
+        database.open()
+        try database.executeUpdate(sql, values: values)
         database.close()
     }
 }
