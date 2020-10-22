45f8d973bdc1040e85ba4cfe99d02212e3a304ca
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-22 23:48:24 +0900

    implement: store  publisher

--- DatabaseComparison/Stores/GRDB/GRDBStore.swift
+++ DatabaseComparison/Stores/GRDB/GRDBStore.swift
@@ -8,4 +8,33 @@ final class GRDBPublisherStore {
     init (databaseQueue: DatabaseQueue) {
         self.databaseQueue = databaseQueue
     }
+
+    func create(publisher: Publisher) {
+        try? databaseQueue.write { database in
+            var owner = publisher.owner
+            try? owner.insert(database)
+            publisher.books.forEach { book in
+                var book = book
+                try? book.insert(database)
+            }
+            var publisherInfo = GRDBStoredPublisher(
+                id: publisher.id,
+                name: publisher.name,
+                ownerId: publisher.owner.id
+            )
+            try? publisherInfo.insert(database)
+        }
+    }
+
+    func read() {
+
+    }
+
+    func update(publisher: Publisher) {
+
+    }
+
+    func delete(publisher: Publisher) {
+        
+    }
 }
