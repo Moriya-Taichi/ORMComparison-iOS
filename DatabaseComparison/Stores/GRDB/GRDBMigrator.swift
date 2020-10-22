421e43efc4ff34e55f999d7a0df6acdf29b78723
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-22 23:10:42 +0900

    implement: migrator

--- DatabaseComparison/Stores/GRDB/GRDBMigrator.swift
+++ DatabaseComparison/Stores/GRDB/GRDBMigrator.swift
@@ -5,4 +5,37 @@
 //  Created by Mori on 2020/10/22.
 //
 
-import Foundation
+import GRDB
+
+final class GRDBMigrator {
+
+    private var migrator = DatabaseMigrator()
+    private let database: DatabaseQueue
+
+    init (database: DatabaseQueue) {
+        self.database = database
+    }
+
+    func migrate() {
+        migrateV1()
+        migrateV2()
+        do {
+            try migrator.migrate(database)
+        }
+        catch {
+            print(error)
+        }
+    }
+
+    private func migrateV1() {
+        migrator.registerMigration("v1") { database in
+            // something do on v1
+        }
+    }
+
+    private func migrateV2() {
+
+    }
+}
+
+
