0d28227dd8b9e8e4fc3f36b959c315a66802b4a7
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-23 15:28:31 +0900

    implement: read

--- DatabaseComparison/Stores/GRDB/GRDBStore.swift
+++ DatabaseComparison/Stores/GRDB/GRDBStore.swift
@@ -14,7 +14,12 @@ final class GRDBPublisherStore {
             var owner = publisher.owner
             try? owner.insert(database)
             publisher.books.forEach { book in
-                var book = book
+                var book = GRDBStoredBook(
+                    id: book.id,
+                    name: book.name,
+                    price: book.price,
+                    publisherId: publisher.id
+                )
                 try? book.insert(database)
             }
             var publisherInfo = GRDBStoredPublisher(
@@ -26,14 +31,25 @@ final class GRDBPublisherStore {
         }
     }
 
-    func read() {
-
+    func read() -> [Publisher]? {
+        return databaseQueue.read { database in
+            let request = GRDBStoredPublisher
+                .including(all: GRDBStoredPublisher.books)
+                .including(required: GRDBStoredPublisher.owner)
+            return try? Publisher.fetchAll(database, request)
+        }
     }
 
     func update(publisher: Publisher) {
         try? databaseQueue.write { database in
             try? publisher.owner.update(database)
             publisher.books.forEach { book in
+                let book = GRDBStoredBook(
+                    id: book.id,
+                    name: book.name,
+                    price: book.price,
+                    publisherId: publisher.id
+                )
                 try? book.update(database)
             }
             let publisherInfo = GRDBStoredPublisher(
@@ -49,6 +65,12 @@ final class GRDBPublisherStore {
         try? databaseQueue.write { database in
             try? publisher.owner.delete(database)
             publisher.books.forEach { book in
+                let book = GRDBStoredBook(
+                    id: book.id,
+                    name: book.name,
+                    price: book.price,
+                    publisherId: publisher.id
+                )
                 try? book.delete(database)
             }
             let publisherInfo = GRDBStoredPublisher(
