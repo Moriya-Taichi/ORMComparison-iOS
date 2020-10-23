46eadafccb5faa9a53515f6610bb147fd9df9c54
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-23 15:28:07 +0900

    fix: publisher and book model

--- DatabaseComparison/Stores/GRDB/GRDBModelExtention.swift
+++ DatabaseComparison/Stores/GRDB/GRDBModelExtention.swift
@@ -7,16 +7,60 @@
 
 import GRDB
 
-extension Book: MutablePersistableRecord, FetchableRecord {
+struct GRDBStoredPublisher: Decodable {
+    let id: Int
+    let name: String
+    let ownerId: Int
+}
 
+struct GRDBStoredBook: Decodable {
+    let id: Int
+    let name: String
+    let price: Int
+    let publisherId: Int
+}
+
+extension Publisher: FetchableRecord { }
+
+extension GRDBStoredBook: MutablePersistableRecord, FetchableRecord {
     enum Columns: String, ColumnExpression {
-        case id, name, price
+        case id, name, price, publisherId
     }
 
     func encode(to container: inout PersistenceContainer) {
         container[Columns.id] = id
         container[Columns.name] = name
         container[Columns.price] = price
+        container[Columns.publisherId] = publisherId
+    }
+
+    static let publisher = belongsTo(GRDBStoredPublisher.self)
+    var publisher: QueryInterfaceRequest<GRDBStoredPublisher> {
+        request(for: GRDBStoredBook.publisher)
+    }
+}
+
+extension GRDBStoredPublisher: MutablePersistableRecord, FetchableRecord  {
+
+    enum Columns: String, ColumnExpression {
+        case id, name, ownerId
+    }
+
+    func encode(to container: inout PersistenceContainer) {
+        container[Columns.id] = id
+        container[Columns.name] = name
+        container[Columns.ownerId] = ownerId
+    }
+
+    static let books = hasMany(GRDBStoredBook.self)
+    static let owner = belongsTo(Owner.self)
+
+    var books: QueryInterfaceRequest<GRDBStoredBook> {
+        request(for: Self.books)
+    }
+
+    var owner:QueryInterfaceRequest<Owner> {
+        request(for: Self.owner)
     }
 }
 
@@ -33,5 +77,8 @@ extension Owner: MutablePersistableRecord, FetchableRecord {
         container[Columns.profile] = profile
     }
 
-
+    static let publishers = hasMany(GRDBStoredPublisher.self)
+    var publishers: QueryInterfaceRequest<GRDBStoredPublisher> {
+        request(for: Owner.publishers)
+    }
 }
