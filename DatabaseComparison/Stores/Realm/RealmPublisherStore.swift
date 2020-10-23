0c8518182a8173db618ae3d0248f1d3bcc081d54
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-23 17:28:58 +0900

    implement: delete object and create realm object

--- DatabaseComparison/Stores/Realm/RealmPublisherStore.swift
+++ DatabaseComparison/Stores/Realm/RealmPublisherStore.swift
@@ -1,7 +1,39 @@
 import Foundation
 import RealmSwift
 
-final class RealmStore {
+class PublisherObject: Object {
+    @objc dynamic var id: Int = -1
+    @objc dynamic var name: String = ""
+    @objc dynamic var owner: OwnerObject?
+    let books = List<BookObject>()
+
+    static override func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+class BookObject: Object {
+    @objc dynamic var id: Int = -1
+    @objc dynamic var name: String = ""
+    @objc dynamic var price: Int = -1
+
+    static override func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+class OwnerObject: Object {
+    @objc dynamic var id: Int = -1
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = -1
+    @objc dynamic var profile: String = ""
+
+    static override func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+final class RealmPublisherStore {
 
     let realm: Realm
 
@@ -9,15 +41,28 @@ final class RealmStore {
         self.realm = realm
     }
 
-    func read<T: Object>(t: T){
-        realm.objects(T.self)
-    }
-
-    func create () {
-
-    }
-
-    func delete() {
+    func create (publisher: Publisher) {
         
+    }
+
+    func read() {
+
+    }
+
+    func update(publisher: Publisher) {
+
+    }
+
+    func delete(publisher: Publisher) {
+        guard let storedPublisher = realm.objects(PublisherObject.self).filter("", publisher.id).first else {
+            return
+        }
+        if realm.isInWriteTransaction {
+            try? realm.delete(storedPublisher)
+        } else {
+            try? realm.write {
+                try? realm.delete(storedPublisher)
+            }
+        }
     }
 }
