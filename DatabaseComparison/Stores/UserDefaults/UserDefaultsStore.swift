a3c9d91250af1de56b6d7b5a594c5115c946b2e8
Moriya-Taichi <minecraft0125@gmail.com>
2020-10-27 12:55:58 +0900

    implement: create logic

--- DatabaseComparison/Stores/UserDefaults/UserDefaultsStore.swift
+++ DatabaseComparison/Stores/UserDefaults/UserDefaultsStore.swift
@@ -5,7 +5,10 @@ final class UserDefaultsStore {
     private let defaults = UserDefaults.standard
 
     func create(publisher: Publisher) {
-
+        var storedPublishers = read()
+        storedPublishers.append(publisher)
+        let publishersData = try? JSONEncoder().encode(storedPublishers)
+        defaults.setValue(publishersData, key: .publisher)
     }
 
     func read () -> [Publisher] {
@@ -18,5 +21,11 @@ final class UserDefaultsStore {
 
     func delete (publisher: Publisher) {
 
+    }
+}
+
+extension UserDefaults {
+    func setValue(_ value: Any?, key: DefaultsKey) {
+        self.setValue(value, forKey: key.rawValue)
     }
 }
