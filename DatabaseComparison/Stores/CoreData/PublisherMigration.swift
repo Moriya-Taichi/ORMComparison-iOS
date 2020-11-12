5136cf36be51bf1ffe931896888626f6b22b6d48
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-12 16:56:29 +0900

    implement: example migration

--- DatabaseComparison/Stores/CoreData/PublisherMigration.swift
+++ DatabaseComparison/Stores/CoreData/PublisherMigration.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import CoreData
+
+final class PublisherMigration: NSEntityMigrationPolicy {
+    override func createDestinationInstances(
+        forSource sInstance: NSManagedObject,
+        in mapping: NSEntityMapping,
+        manager: NSMigrationManager
+    ) throws {
+        //Migration
+        if sInstance.entity.name == "Publisher" {
+            let context = manager.destinationContext
+
+            let publisherName = sInstance.primitiveValue(forKey: "name") as? String
+            let publisherID = sInstance.primitiveValue(forKey: "id") as? Int
+
+            //example
+            //let newObject = Hoge(context: context)
+            //newObject.hoge = publisherName
+            //context.insert(newObject)
+        }
+    }
+}
