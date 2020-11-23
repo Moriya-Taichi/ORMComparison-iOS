92f99cfbbdf7fe8f8d47e605b025f88f9b3b02e8
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-23 19:13:10 +0900

    implement: add viewmodel file and base viewmodel

---
+++ DatabaseComparison/PublisherListViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  PublisherListViewModel.swift
+//  DatabaseComparison
+//
+//  Created by Mori on 2020/11/23.
+//
+
+import Foundation
+
+final class PublisherListViewModel {
+
+    private let store: PublisherStore
+
+    init (store: PublisherStore) {
+        self.store = store
+    }
+
+    func load() {
+
+    }
+    
+    func update() {
+
+    }
+
+    func delete () {
+
+    }
+}
+
