a0c76afd8b51b86d4e280c572fc1f92105af965b
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-23 18:14:54 +0900

    implement: container

--- DatabaseComparison/Container.swift
+++ DatabaseComparison/Container.swift
@@ -5,4 +5,30 @@
 //  Created by Mori on 2020/11/23.
 //
 
+import RealmSwift
 import Foundation
+import CoreData
+import GRDB
+
+struct Container {
+    static let coredataContainer = NSPersistentContainer(name: "")
+    private static let fmdbDatabaseWrapper = FMDBDatabaseWrapeer()
+    private static let fmdbBookStore = FMDBBookStore(databaseWrapper: fmdbDatabaseWrapper)
+    private static let fmdbOwnerStore = FMDBOwnerStore(databaseWrapper: fmdbDatabaseWrapper)
+    static let fmdbPublisherStore = FMDBPublisherStore(
+        databaseWrapper: fmdbDatabaseWrapper,
+        bookStore: fmdbBookStore,
+        ownerStore: fmdbOwnerStore
+    )
+
+    private static let grdbDatabaseQueue = try! DatabaseQueue(path: "")
+    static let grdbPublisherStore = GRDBPublisherStore(databaseQueue: grdbDatabaseQueue)
+
+    private static let realmConfiguration = Realm.Configuration(
+        encryptionKey: "hogehoge".data(using: .utf8),
+        schemaVersion: 1,
+        migrationBlock: nil,
+        deleteRealmIfMigrationNeeded: false
+    )
+    static let realm = try! Realm(configuration: realmConfiguration)
+}
