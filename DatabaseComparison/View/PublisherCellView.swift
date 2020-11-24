a35e3c1f727a01e1cc2b8da40d3096e3fa33fa5c
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-24 13:09:54 +0900

    implement: cell view

--- DatabaseComparison/View/PublisherCellView.swift
+++ DatabaseComparison/View/PublisherCellView.swift
@@ -5,4 +5,29 @@
 //  Created by Mori on 2020/11/24.
 //
 
-import Foundation
+import UIKit
+
+final class PublisherCellView: UIView {
+
+    @IBOutlet weak var publisherNameLabel: UILabel!
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    @IBOutlet weak var bookCountLabel: UILabel!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNib()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    func configure(_ publisher: Publisher) {
+        ownerNameLabel.text = publisher.owner.name
+        publisherNameLabel.text = publisher.name
+        bookCountLabel.text = String(publisher.books.count)
+    }
+}
+
+
