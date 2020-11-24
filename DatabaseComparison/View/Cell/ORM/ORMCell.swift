7891d47a1d033be869806814b61a3e4b56351955
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-24 21:23:23 +0900

    implement: ORM Cell

--- DatabaseComparison/View/Cell/ORM/ORMCell.swift
+++ DatabaseComparison/View/Cell/ORM/ORMCell.swift
@@ -5,4 +5,21 @@
 //  Created by Mori on 2020/11/24.
 //
 
-import Foundation
+import UIKit
+
+final class ORMCell: UICollectionViewCell {
+
+    @IBOutlet weak var ormCellView: ORMCellView!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    func configure(_ ormType: ORMType) {
+        ormCellView.configure(ormType)
+    }
+}
