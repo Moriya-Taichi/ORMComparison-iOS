f175b51a08bede030739b15aadca640c33847a09
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-25 18:58:25 +0900

    implement: base logic

--- DatabaseComparison/ViewModel/PublisherListViewModel.swift
+++ DatabaseComparison/ViewModel/PublisherListViewModel.swift
@@ -5,26 +5,45 @@
 //  Created by Mori on 2020/11/23.
 //
 
-import Foundation
+import Combine
 
 final class PublisherListViewModel {
 
     private let store: PublisherStore
+    private var anyCancellable = Set<AnyCancellable>()
+    private let publishersState = CurrentValueSubject<[Publisher], Never>([])
+
+    let loadInput = PassthroughSubject<Void, Never>()
+    let selectInput = PassthroughSubject<Int, Never>()
+
+    let publishersOutput = PassthroughSubject<[Publisher], Never>()
+    let selectPublisherOutput = PassthroughSubject<Publisher, Never>()
 
     init (store: PublisherStore) {
         self.store = store
-    }
 
-    func load() {
+        loadInput
+            .sink { [weak self] _ in
+                let publishers = store.read()
+                self?.publishersState.value = publishers
+            }
+            .store(in: &anyCancellable)
 
-    }
-    
-    func update() {
+        publishersState
+            .sink { [weak self] publishers in
+                self?.publishersOutput.send(publishers)
+            }
+            .store(in: &anyCancellable)
 
-    }
-
-    func delete () {
-
+        selectInput
+            .sink { [weak self] index in
+                guard let self = self else {
+                    return
+                }
+                let publisher = self.publishersState.value[index]
+                self.selectPublisherOutput.send(publisher)
+            }
+            .store(in: &anyCancellable)
     }
 }
 
