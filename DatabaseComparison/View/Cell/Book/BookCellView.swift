514a2fd5dfc8517120734d088769b04a70529924
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-26 13:09:59 +0900

    implement: book cell view

--- DatabaseComparison/View/Cell/Book/BookCellView.swift
+++ DatabaseComparison/View/Cell/Book/BookCellView.swift
@@ -5,4 +5,25 @@
 //  Created by Mori on 2020/11/24.
 //
 
-import Foundation
+import UIKit
+
+final class BookCellView: UIView {
+
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var bookPriceLabel: UILabel!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNib()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    func configure(_ book: Book) {
+        bookNameLabel.text = book.name
+        bookPriceLabel.text = String(book.price)
+    }
+}
