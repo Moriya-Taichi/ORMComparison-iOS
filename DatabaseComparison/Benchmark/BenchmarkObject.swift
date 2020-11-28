bbe0ddd49e9ca48d052a744ece7f53e26ee3c3fc
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-28 22:15:54 +0900

    implement: objects for Benchmark

--- DatabaseComparison/Benchmark/BenchmarkObject.swift
+++ DatabaseComparison/Benchmark/BenchmarkObject.swift
@@ -5,4 +5,53 @@
 //  Created by Mori on 2020/11/27.
 //
 
-import Foundation
+import RealmSwift
+import GRDB
+
+struct BenchmarkSimplyObject: Codable {
+    let id: Int
+    let name: String
+}
+
+final class BenchmarkSimplyRealmObject: Object {
+    @objc dynamic var id: Int = 0
+    @objc dynamic var name: String = ""
+
+    static override func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+struct BenchmarkOneToOneObject: Codable {
+    let id: Int
+    let name: String
+    let realtionObject: BenchmarkSimplyObject
+}
+
+final class BenchmarkOneToOneRealmObject: Object {
+    @objc dynamic var id: Int = 0
+    @objc dynamic var name: String = ""
+    @objc dynamic var relationObject: BenchmarkSimplyRealmObject?
+
+    static override func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+struct BenchmarkOneToManyObject: Codable {
+    let id: Int
+    let name: String
+    let relationObjects: [BenchmarkSimplyObject]
+}
+
+final class BenchmarkOneToManyRealmObject: Object {
+    @objc dynamic var id: Int = 0
+    @objc dynamic var name: String = ""
+    let relationObjects = List<BenchmarkSimplyRealmObject>()
+
+    static override func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+
