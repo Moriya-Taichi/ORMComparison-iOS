ed4e7be02b31738a298ccd614c4f9fb504e311d4
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-28 22:15:27 +0900

    add benchmark file

---
+++ DatabaseComparison/Benchmark/BenchmarkObject.swift
@@ -0,0 +1,8 @@
+//
+//  BenchmarkObject.swift
+//  DatabaseComparison
+//
+//  Created by Mori on 2020/11/27.
+//
+
+import Foundation
---
+++ DatabaseComparison/Benchmark/Benchmarker.swift
@@ -0,0 +1,15 @@
+//
+//  BenchmarkStore.swift
+//  DatabaseComparison
+//
+//  Created by Mori on 2020/11/27.
+//
+
+import CoreData
+import Realm
+import GRDB
+import FMDB
+
+final class BenchmarkStore {
+    
+}
