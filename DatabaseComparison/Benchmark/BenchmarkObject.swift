fd12f4c5b01453e560e476c7318f3a7610e45b1c
Moriya-Taichi <minecraft0125@gmail.com>
2020-11-29 19:50:57 +0900

    fix: GRDB Model

--- DatabaseComparison/Benchmark/BenchmarkObject.swift
+++ DatabaseComparison/Benchmark/BenchmarkObject.swift
@@ -8,12 +8,15 @@
 import RealmSwift
 import GRDB
 
-struct BenchmarkSimplyObject: Codable {
+struct SimplyObject:
+    Codable,
+    FetchableRecord,
+    PersistableRecord {
     let id: Int
     let name: String
 }
 
-final class BenchmarkSimplyRealmObject: Object {
+final class SimplyRealmObject: Object {
     @objc dynamic var id: Int = 0
     @objc dynamic var name: String = ""
 
@@ -22,32 +25,80 @@ final class BenchmarkSimplyRealmObject: Object {
     }
 }
 
-struct BenchmarkOneToOneObject: Codable {
+struct OneToOneObject:
+    Codable,
+    FetchableRecord,
+    PersistableRecord {
     let id: Int
     let name: String
-    let realtionObject: BenchmarkSimplyObject
+    let realtionObject: SimplyObject
 }
 
-final class BenchmarkOneToOneRealmObject: Object {
+struct OneToOneGRDBObject:
+    Codable,
+    FetchableRecord,
+    PersistableRecord {
+    let id: Int
+    let name: String
+    static let relationObject = hasOne(OneToOneChildGRDBObject.self)
+    var relationObject: QueryInterfaceRequest<OneToOneChildGRDBObject> {
+        request(for: Self.relationObject)
+    }
+}
+
+struct OneToOneChildGRDBObject:
+    Codable,
+    FetchableRecord,
+    PersistableRecord {
+    let id: Int
+    let name: String
+    let oneToOneGRDBObjectId: Int
+    static let parentObject = belongsTo(OneToOneGRDBObject.self)
+    var parentObject: QueryInterfaceRequest<OneToOneGRDBObject> {
+        request(for: Self.parentObject)
+    }
+}
+
+final class OneToOneRealmObject: Object {
     @objc dynamic var id: Int = 0
     @objc dynamic var name: String = ""
-    @objc dynamic var relationObject: BenchmarkSimplyRealmObject?
+    @objc dynamic var relationObject: SimplyRealmObject?
 
     static override func primaryKey() -> String? {
         return "id"
     }
 }
 
-struct BenchmarkOneToManyObject: Codable {
+struct OneToManyObject: Codable, FetchableRecord, PersistableRecord {
     let id: Int
     let name: String
-    let relationObjects: [BenchmarkSimplyObject]
+    let relationObjects: [SimplyObject]
 }
 
-final class BenchmarkOneToManyRealmObject: Object {
+struct OneToManyGRDBObject: Codable, FetchableRecord, PersistableRecord {
+    let id: Int
+    let name: String
+
+    static let relationObjects = hasMany(OneToManyChildGRDBObject.self)
+    var relationObjects: QueryInterfaceRequest<OneToManyChildGRDBObject> {
+        request(for: Self.relationObjects)
+    }
+}
+
+struct OneToManyChildGRDBObject: Codable, FetchableRecord, PersistableRecord {
+    let id: Int
+    let name: String
+
+    static let parentObject = belongsTo(OneToManyGRDBObject.self)
+    var parentObject: QueryInterfaceRequest<OneToManyGRDBObject> {
+        request(for: Self.parentObject)
+    }
+}
+
+final class OneToManyRealmObject: Object {
     @objc dynamic var id: Int = 0
     @objc dynamic var name: String = ""
-    let relationObjects = List<BenchmarkSimplyRealmObject>()
+    let relationObjects = List<SimplyRealmObject>()
 
     static override func primaryKey() -> String? {
         return "id"
