cf55e873ccf5556316c92e4e029155301e0ce38c
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-02 14:31:15 +0900

    implement: create table

--- DatabaseComparison/Benchmark/Benchmarker.swift
+++ DatabaseComparison/Benchmark/Benchmarker.swift
@@ -1,15 +1,124 @@
 //
-//  BenchmarkStore.swift
+//  Benchmarker.swift
 //  DatabaseComparison
 //
 //  Created by Mori on 2020/11/27.
 //
 
+import RealmSwift
 import CoreData
-import Realm
 import GRDB
 import FMDB
 
-final class BenchmarkStore {
-    
+final class Benchmarker {
+
+    private let fmDatabasePool: FMDatabasePool
+    private let databasePool: DatabasePool
+    private let userDefaults: UserDefaults
+    private let container: NSPersistentContainer
+    private let realm: Realm
+
+    init(
+        fmDatabasePool: FMDatabasePool,
+        databasePool: DatabasePool,
+        userDefaults: UserDefaults,
+        container: NSPersistentContainer,
+        realm: Realm
+    ) {
+        self.fmDatabasePool = fmDatabasePool
+        self.databasePool = databasePool
+        self.userDefaults = userDefaults
+        self.container = container
+        self.realm = realm
+
+        try? databasePool.write { database in
+            try? database.create(
+                table: SimplyObject.databaseTableName,
+                temporary: false,
+                ifNotExists: true
+            ) { table in
+                table.column("id", .integer)
+                    .notNull()
+                    .indexed()
+                table.column("name", .text).notNull()
+                table.primaryKey(["id"])
+            }
+        }
+
+        try? databasePool.write { database in
+            try? database.create(
+                table: GRDBObject
+                    .OneToOneObject
+                    .databaseTableName,
+                temporary: false,
+                ifNotExists: true
+            ) { table in
+                table.column("id", .integer)
+                    .notNull()
+                    .indexed()
+                table.column("name", .text).notNull()
+                table.primaryKey(["id"])
+            }
+        }
+
+        try? databasePool.write { database in
+            try? database.create(
+                table: GRDBObject
+                    .OneToOneChildObject
+                    .databaseTableName,
+                temporary: false,
+                ifNotExists: true
+            ) { table in
+                table.column("id", .integer).notNull()
+                table.column("name", .text).notNull()
+                table.column("oneToOneObjectId", .integer)
+                    .notNull()
+                    .indexed()
+                    .references(
+                        GRDBObject
+                            .OneToOneObject
+                            .databaseTableName
+                    )
+                table.primaryKey(["id"])
+            }
+        }
+
+        try? databasePool.write { database in
+            try? database.create(
+                table: GRDBObject
+                    .OneToManyObject
+                    .databaseTableName,
+                temporary: false,
+                ifNotExists: true
+            ) { table in
+                table.column("id", .integer)
+                    .notNull()
+                    .indexed()
+                table.column("name", .text).notNull()
+                table.primaryKey(["id"])
+            }
+        }
+
+        try? databasePool.write { database in
+            try? database.create(
+                table: GRDBObject
+                    .OneToManyChildObject
+                    .databaseTableName,
+                temporary: false,
+                ifNotExists: true
+            ) { table in
+                table.column("id", .integer).notNull()
+                table.column("name", .text).notNull()
+                table.column("oneToManyObjectId", .integer)
+                    .notNull()
+                    .indexed()
+                    .references(
+                        GRDBObject
+                            .OneToManyObject
+                            .databaseTableName
+                    )
+                table.primaryKey(["id"])
+            }
+        }
+    }
 }
