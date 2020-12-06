0ea36ef4c30e5bfe8382c41bf1b328316e98d6a2
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-07 01:23:20 +0900

    implement: container for benchmark

--- DatabaseComparison/Benchmark/BenchmarkContainer.swift
+++ DatabaseComparison/Benchmark/BenchmarkContainer.swift
@@ -5,4 +5,32 @@
 //  Created by Mori on 2020/12/07.
 //
 
+import RealmSwift
 import Foundation
+import CoreData
+import FMDB
+import GRDB
+
+struct BenchmarkContainer {
+    private static let container = NSPersistentContainer(name: "benchmark")
+    private static let realmConfiguration = Realm.Configuration(
+        fileURL: .init(fileURLWithPath: "/realm/benchmark"),
+        schemaVersion: 1,
+        deleteRealmIfMigrationNeeded: true,
+        objectTypes: [
+            RealmObject.SimplyObject.self,
+            RealmObject.OneToOneObject.self,
+            RealmObject.OneToManyObject.self
+        ]
+    )
+    private static let realm = try! Realm(configuration: realmConfiguration)
+    private static let databasePool = try! DatabasePool(path: "/GRDB/benchmark")
+    private static let fmDatabasePool = FMDatabasePool(path: "/FMDB/benchmark")
+    static let benchmarker = Benchmarker(
+        fmDatabasePool: fmDatabasePool,
+        databasePool: databasePool,
+        userDefaults: .standard,
+        container: container,
+        realm: realm
+    )
+}
