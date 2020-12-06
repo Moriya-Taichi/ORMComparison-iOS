3f2f440dfc8729a2dccfc78a38680eb36f24e67c
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-07 02:51:27 +0900

    implement: benchmark view base layout

--- DatabaseComparison/View/ViewController/Benchmark/BenchmarkViewController.swift
+++ DatabaseComparison/View/ViewController/Benchmark/BenchmarkViewController.swift
@@ -5,4 +5,25 @@
 //  Created by Mori on 2020/12/07.
 //
 
-import Foundation
+import UIKit
+
+final class BenchmarkViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton! {
+        didSet {
+            startButton.layer.cornerRadius = startButton.frame.height / 2
+            startButton.backgroundColor = .systemBlue
+            startButton.tintColor = .white
+            startButton.titleLabel?.font = .systemFont(
+                ofSize: 18,
+                weight: .semibold
+            )
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "ベンチマーク"
+    }
+}
