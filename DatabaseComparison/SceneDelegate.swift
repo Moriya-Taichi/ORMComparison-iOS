e9dde4bfa910256b4ac39542dd7dc40365f5ce52
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-07 02:48:30 +0900

    fix: replace start viewcontroller

--- DatabaseComparison/SceneDelegate.swift
+++ DatabaseComparison/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let navigationController = UINavigationController()
-        navigationController.viewControllers = [ORMListViewController()]
+        navigationController.viewControllers = [SelectViewController()]
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
