5fca96d21d892724142aea1ab682df314c468483
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-07 00:55:27 +0900

    fix: use navigation controller

--- DatabaseComparison/SceneDelegate.swift
+++ DatabaseComparison/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = ORMListViewController()
+        let navigationController = UINavigationController()
+        navigationController.viewControllers = [ORMListViewController()]
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
