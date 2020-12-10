9d27dbbce1b7bd8565d3f97c5a0de9171a922b68
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-10 16:28:31 +0900

    fix: path

--- DatabaseComparison/Container.swift
+++ DatabaseComparison/Container.swift
@@ -11,7 +11,7 @@ import CoreData
 import GRDB
 
 struct Container {
-    private static let coredataContainer = NSPersistentContainer(name: "")
+    private static let coredataContainer = NSPersistentContainer(name: "DatabaseComparison")
     private static let fmdbDatabaseWrapper = FMDBDatabaseWrapeer()
     private static let fmdbBookStore = FMDBBookStore(databaseWrapper: fmdbDatabaseWrapper)
     private static let fmdbOwnerStore = FMDBOwnerStore(databaseWrapper: fmdbDatabaseWrapper)
@@ -22,7 +22,7 @@ struct Container {
         deleteRealmIfMigrationNeeded: false
     )
     private static let realm = try! Realm(configuration: realmConfiguration)
-    private static let grdbDatabaseQueue = try! DatabaseQueue(path: "")
+    private static let grdbDatabaseQueue = try! DatabaseQueue(path: "grdbDatabase.sqlite")
     static let grdbPublisherStore = GRDBPublisherStore(databaseQueue: grdbDatabaseQueue)
     static let fmdbPublisherStore = FMDBPublisherStore(
         databaseWrapper: fmdbDatabaseWrapper,
