d2a2a083ba41f4e904ab791406ea93c5ef22ec87
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-16 21:23:41 +0900

    fix:remove select screen

--- DatabaseComparison/SceneDelegate.swift
+++ DatabaseComparison/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let navigationController = UINavigationController()
-        navigationController.viewControllers = [SelectViewController()]
+        navigationController.viewControllers = [ORMListViewController()]
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
