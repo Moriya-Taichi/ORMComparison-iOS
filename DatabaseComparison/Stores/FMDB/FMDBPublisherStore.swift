c853037ac38df220a5fc1e0cd9e27b5054d8144c
Moriya-Taichi <minecraft0125@gmail.com>
2020-12-16 21:26:04 +0900

    fix: all logic

--- DatabaseComparison/Stores/FMDB/FMDBPublisherStore.swift
+++ DatabaseComparison/Stores/FMDB/FMDBPublisherStore.swift
@@ -2,114 +2,179 @@ import FMDB
 
 final class FMDBPublisherStore: PublisherStore {
 
-    private let databaseWrapper: FMDBDatabaseWrapeer
-    private let bookStore: FMDBBookStore
-    private let ownerStore: FMDBOwnerStore
-
-    private let createTableSQL =
-        "CREATE TABLE IF NOT EXISTS publishers (" +
-        "id INTEGER PRIMARY KEY, " +
-        "name TEXT, " +
-        "owner_id INTEGER" +
-      ");"
-
-    private let insertSQL =
-        "INSERT INTO " +
-        "publishers (id, name, owner_id) " +
-        "VALUES " +
-        "(?, ?, ?);"
-
-    private let selectSQL =
-        "SELECT " +
-        "id, name, owner_id " +
-        "FROM " +
-        "publishers;" +
-        "ORDER BY id;"
-
-    private let updateSQL =
-        "UPDATE " +
-        "publishers " +
-        "SET " +
-        "name = ?, owner_id = ? " +
-        "WHERE " +
-        "id = ?;"
-
-    private let deleteSQL =
-        "DELETE FROM publishers WHERE id = ?;"
-
+    private let fmDatabaseQueue: FMDatabaseQueue
 
     init (
-        databaseWrapper: FMDBDatabaseWrapeer,
-        bookStore: FMDBBookStore,
-        ownerStore: FMDBOwnerStore
+        fmDatabaseQueue: FMDatabaseQueue
     ) {
-        self.databaseWrapper = databaseWrapper
-        self.bookStore = bookStore
-        self.ownerStore = ownerStore
-        try? databaseWrapper
-            .executeUpdate(createTableSQL, values: nil)
+        self.fmDatabaseQueue = fmDatabaseQueue
+        fmDatabaseQueue.inDatabase { database in
+            let createPublisherTableSQL = "CREATE TABLE IF NOT EXISTS publishers (id INTEGER PRIMARY KEY, name TEXT, owner_id INTEGER, foreign key(owner_id) references owners(id));"
+            let createBooksTableSQL = "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, name TEXT, price INTEGER, publisher_id INTEGER, foreign key(publisher_id) references publishers(id));"
+            let createOwnersTableSQL = "CREATE TABLE IF NOT EXISTS owners (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, profile TEXT);"
+            database.open()
+            try? database.executeUpdate(
+                createOwnersTableSQL,
+                values: nil
+            )
+
+            try? database.executeUpdate(
+                createPublisherTableSQL,
+                values: nil
+            )
+
+            try? database.executeUpdate(
+                createBooksTableSQL,
+                values: nil
+            )
+            database.close()
+        }
     }
 
     func create(publisher: Publisher) {
-        try? databaseWrapper
-            .executeUpdate(
-            insertSQL,
-            values: [
-                publisher.id,
-                publisher.name,
-                publisher.owner.id
-            ]
-        )
-        ownerStore.create(owner: publisher.owner)
-        bookStore.createBooks(books: publisher.books, publisherID: publisher.id)
+        let createPublisherSQL = "INSERT INTO publishers (id, name, owner_id) VALUES (?, ?, ?);"
+        let createBookSQL = "INSERT INTO books (id, name, price, publisher_id) VALUES " +
+            Array(
+                repeating: "(?, ?, ?, ?)",
+                count: publisher.books.count
+            )
+            .joined(separator: ",") +
+            ";"
+        let createOwnerSQL = "INSERT INTO owners (id, name, age, profile) VALUES (?, ?, ?, ?);"
+        fmDatabaseQueue.inDatabase { database in
+            try? database.executeUpdate(
+                createOwnerSQL,
+                values: [
+                    publisher.owner.id,
+                    publisher.owner.name,
+                    publisher.owner.age,
+                    publisher.owner.profile
+                ]
+            )
+
+            try? database.executeUpdate(
+                createPublisherSQL,
+                values: [
+                    publisher.id,
+                    publisher.name,
+                    publisher.owner.id
+                ]
+            )
+
+            try? database.executeUpdate(
+                createBookSQL,
+                values: publisher.books.map { book -> [Any] in
+                    return [
+                        book.id,
+                        book.name,
+                        book.price,
+                        publisher.id
+                    ]
+                }.flatMap { $0 }
+            )
+        }
     }
 
     func readByLazyLoading () -> [Publisher] {
-        var temporaryPublishers: [(
-            id: Int,
-            name: String,
-            ownerId: Int
-        )] = []
-        if
-            let result = try? databaseWrapper
-                .executeQuery(
-                selectSQL,
+        let selectPublishersSQL = "SELECT * FROM publishers;"
+        var publishers: [Publisher] = []
+        fmDatabaseQueue.inDatabase { database in
+            database.open()
+            var rowPublishers: [(id: Int, name: String, ownerID: Int)] = []
+            var rowBooks: [(id: Int, name: String, price: Int, publisherID: Int)] = []
+            var rowOwners: [(id: Int, name: String, age: Int, profile: String)] = []
+            if let result = try? database.executeQuery(
+                selectPublishersSQL,
                 values: nil
-        ) {
-            while result.next() {
-                temporaryPublishers.append(
-                    (
-                        id: result.long(forColumnIndex: 0),
-                        name: result.string(forColumnIndex: 0) ?? "",
-                        ownerId: result.long(forColumnIndex: 0)
+            ) {
+                while result.next() {
+                    rowPublishers.append(
+                        (
+                            id: Int(result.int(forColumnIndex: 0)),
+                            name: result.string(forColumnIndex: 1) ?? "",
+                            ownerID: Int(result.int(forColumnIndex: 2))
+                         )
+                    )
+                }
+            }
+
+            guard !rowPublishers.isEmpty else {
+                return
+            }
+
+            let selectOwnersSQL = "SELECT * FROM owners WHERE id IN (" +
+                Array(
+                    repeating: "?",
+                    count: rowPublishers.count
+                ).joined(separator: ",") +
+                ");"
+            let selectBooksSQL = "SELECT * FROM books WHERE publisher_id IN (" +
+                Array(
+                    repeating: "?",
+                    count: rowPublishers.count
+                )
+                .joined(separator: ",") +
+                ");"
+
+            if let result = try? database.executeQuery(
+                selectOwnersSQL,
+                values: nil
+            ) {
+                while result.next() {
+                    rowOwners.append(
+                        (
+                            id: Int(result.int(forColumnIndex: 0)),
+                            name: result.string(forColumnIndex: 1) ?? "",
+                            age: Int(result.int(forColumnIndex: 2)),
+                            profile: result.string(forColumnIndex: 3) ?? ""
+                         )
+                    )
+                }
+            }
+
+            if let result = try? database.executeQuery(
+                selectBooksSQL,
+                values: nil
+            ) {
+                while result.next() {
+                    rowBooks.append(
+                        (
+                            id: Int(result.int(forColumnIndex: 0)),
+                            name: result.string(forColumnIndex: 1) ?? "",
+                            price: Int(result.int(forColumnIndex: 2)),
+                            publisherID: Int(result.int(forColumnIndex: 3))
+                         )
+                    )
+                }
+            }
+
+            rowPublishers.forEach { publisher in
+                let rowOwner = rowOwners.first { owner -> Bool in
+                    return owner.id == publisher.ownerID
+                }!
+
+                let owner = Owner(
+                    id: rowOwner.id,
+                    name: rowOwner.name,
+                    age: rowOwner.age,
+                    profile: rowOwner.profile
+                )
+                let books = rowBooks
+                    .filter { $0.publisherID == publisher.id }
+                    .map { Book(id: $0.id, name: $0.name, price: $0.price) }
+                publishers.append(
+                    .init(
+                        id: publisher.id,
+                        name: publisher.name,
+                        books: books,
+                        owner: owner
                     )
                 )
             }
-        }
 
-        let publisherIDs = temporaryPublishers.map { $0.id }
-        let books = bookStore.readBooksWithIDByPublisherIDs(publisherIDs)
-        let ownerIDs = temporaryPublishers.map { $0.ownerId }
-        let owners = ownerStore.readByIDs(Array(Set(ownerIDs)))
-        
-        return temporaryPublishers.compactMap { publisher -> Publisher? in
-            guard
-                let owner = owners
-                    .filter({ $0.id == publisher.ownerId })
-                    .first
-            else {
-                return nil
-            }
-            let book = books
-                .filter { $0.id == publisher.id }
-                .map { $0.book }
-            return Publisher(
-                id: publisher.id,
-                name: publisher.name,
-                books: book,
-                owner: owner
-            )
+            database.close()
         }
+        return publishers
     }
 
     func read() -> [Publisher] {
@@ -117,68 +182,116 @@ final class FMDBPublisherStore: PublisherStore {
         let query = "SELECT publishers.id, publishers.name, books.id, books.name, books.price, owners.id, owners.name, owners.age, owners.profile FROM publishers " +
             "LEFT JOIN books ON publishers.id == books.publisher_id " +
             "LEFT JOIN owners ON publishers.owner_id == owners.id;"
-        if
-            let result = try? databaseWrapper.executeQuery(
-                query,
-                values: nil
-            ) {
-            var currentPublisher = Publisher(
-                id: 0,
-                name: "",
-                books: [],
-                owner: .init(
-                    id: 0,
+        fmDatabaseQueue.inDatabase { database in
+            if
+                let result = try? database.executeQuery(
+                    query,
+                    values: nil
+                ) {
+                var currentPublisher = Publisher(
+                    id: 1,
                     name: "",
-                    age: 0,
-                    profile: "")
-            )
-            var books: [Book] = []
-            while result.next() {
-                if currentPublisher.id != Int(result.int(forColumnIndex: 0)) {
-                    publishers.append(currentPublisher)
-                    books = []
-                }
-
-                let book = Book(
-                    id: Int(result.int(forColumnIndex: 2)),
-                    name: result.string(forColumnIndex: 3) ?? "",
-                    price: Int(result.int(forColumnIndex: 4))
-                )
-                books.append(book)
-
-                currentPublisher = Publisher(
-                    id: Int(result.int(forColumnIndex: 0)),
-                    name: result.string(forColumnIndex: 1) ?? "",
-                    books: books,
+                    books: [],
                     owner: .init(
-                        id: Int(result.int(forColumnIndex: 5)),
-                        name: result.string(forColumnIndex: 6) ?? "",
-                        age: Int(result.int(forColumnIndex: 7)),
-                        profile: result.string(forColumnIndex: 8) ?? ""
-                    )
+                        id: 0,
+                        name: "",
+                        age: 0,
+                        profile: "")
                 )
+                var books: [Book] = []
+                while result.next() {
+                    if currentPublisher.id != Int(result.int(forColumnIndex: 0)) {
+                        publishers.append(currentPublisher)
+                        books = []
+                    }
+
+                    let book = Book(
+                        id: Int(result.int(forColumnIndex: 2)),
+                        name: result.string(forColumnIndex: 3) ?? "",
+                        price: Int(result.int(forColumnIndex: 4))
+                    )
+                    books.append(book)
+
+                    currentPublisher = Publisher(
+                        id: Int(result.int(forColumnIndex: 0)),
+                        name: result.string(forColumnIndex: 1) ?? "",
+                        books: books,
+                        owner: .init(
+                            id: Int(result.int(forColumnIndex: 5)),
+                            name: result.string(forColumnIndex: 6) ?? "",
+                            age: Int(result.int(forColumnIndex: 7)),
+                            profile: result.string(forColumnIndex: 8) ?? ""
+                        )
+                    )
+                }
             }
         }
         return publishers
     }
 
     func update (publisher: Publisher) {
-        try? databaseWrapper
-            .executeUpdate(
-            updateSQL,
-            values: [
-                publisher.name,
-                publisher.owner.id,
-                publisher.id
-            ]
-        )
+        let updataPublisherSQL = "UPDATE publishers SET name = ?, owner_id = ? WHERE id = ?;"
+        let updateBookSQL =
+            "UPDATE books SET name = ?, price = ?, publisher_id = ? WHERE id = ?;"
+        let updateOwnerSQL = "UPDATE owners SET name = ?, age = ?, profile = ? WHERE id = ?;"
+        fmDatabaseQueue.inDatabase { database in
+            try? database
+                .executeUpdate(
+                updataPublisherSQL,
+                values: [
+                    publisher.name,
+                    publisher.owner.id,
+                    publisher.id
+                ]
+            )
 
-        bookStore.updateByBooks(books: publisher.books, publisherID: publisher.id)
-        ownerStore.update(owner: publisher.owner)
+            try? database
+                .executeUpdate(
+                    updateOwnerSQL,
+                    values: [
+                        publisher.owner.name,
+                        publisher.owner.age,
+                        publisher.owner.profile,
+                        publisher.owner.id
+                    ]
+                )
+
+            if database.beginTransaction() {
+                publisher.books.forEach { book in
+                    try? database.executeUpdate(
+                        updateBookSQL,
+                        values: [
+                            book.name,
+                            book.price,
+                            publisher.id,
+                            book.id
+                        ]
+                    )
+                }
+                database.commit()
+            }
+        }
     }
 
     func delete(publisher: Publisher) {
-        try? databaseWrapper.executeUpdate(deleteSQL, values: [publisher.id])
-        bookStore.deleteByPublisherID(publisherID: publisher.id)
+        let deletePublisherSQL =
+            "DELETE FROM publishers WHERE id = ?;"
+        let deleteBookSQL =
+            "DELETE FROM books WHERE publisher_id = ?;"
+        fmDatabaseQueue.inDatabase { database in
+            try? database.executeUpdate(
+                deletePublisherSQL,
+                values: [
+                    publisher.id
+                ]
+            )
+
+            try? database.executeUpdate(
+                deleteBookSQL,
+                values: [
+                    publisher.id
+                ]
+            )
+        }
     }
 }
